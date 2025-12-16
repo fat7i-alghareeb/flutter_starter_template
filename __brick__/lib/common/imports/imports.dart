3e244d531b82d8ebab312abe7c400f92fdcf243f
Fat7i <fat7ighareeb@gmail.com>
2025-12-16 19:14:40 +0300

    .

--- __brick__/lib/common/imports/imports.dart
+++ __brick__/lib/common/imports/imports.dart
@@ -31,3 +31,6 @@ export "../../utils/helpers/colored_print.dart";
 export "../../utils/helpers/build_svg_icon.dart";
 export "../../core/theme/app_colors.dart";
 export "../../core/theme/app_text_styles.dart";
+
+//assets
+export '../../utils/gen/assets.gen.dart';
